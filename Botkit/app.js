4f082febdecde9757afde8b4ef430ff24ed5a691
Singireddy Bharath <singireddybharath99@gmail.com>
2023-10-19 11:49:17 +0530

    updated botkit

--- Botkit/app.js
+++ Botkit/app.js
@@ -1,8 +1,8 @@
-var Application = require("../lib/app");
-var Server = require("../lib/server");
-var sdk = require("../lib/sdk");
-var config = require("../config");
-require("../helpers/CustomLogger.js");
+var Application = require("./lib/app");
+var Server = require("./lib/server");
+var sdk = require("./lib/sdk");
+var config = require("./config");
+require("./helpers/CustomLogger.js");
 const serverless = require("serverless-http");
 
 var app = new Application(null, config);
@@ -10,7 +10,8 @@ var server = new Server(config, app);
 
 server.start();
 
-sdk.registerBot(require("../RA_EX_Zebra_v2.1.js"));
+sdk.registerBot(require("./RA_EX_Zebra_v2.1.js"));
+
 // app.load();
 // app.use(`/.netlify/functions/api`, server);
 // module.exports.handler = serverless(app);
